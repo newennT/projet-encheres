72acd42c427edaa96f9372b09ad2d19ded86195c
Camptbel <pacary.hassan@gmail.com>
2024-03-11 14:18:41 +0100

    initialisation du projet

---
+++ src/main/java/fr/projet/enchere/projet_grp7_enchere/ProjetGrp7EnchereApplication.java
@@ -0,0 +1,13 @@
+package fr.projet.enchere.projet_grp7_enchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGrp7EnchereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetGrp7EnchereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/projet/enchere/projet_grp7_enchere/ProjetGrp7EnchereApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projet.enchere.projet_grp7_enchere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGrp7EnchereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
